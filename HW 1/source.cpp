1e34c3fa7b093a23b81512c6d71113a31779bbd4
SKT T1 IU <32501438+mathpop09@users.noreply.github.com>
2019-04-19 18:23:02 -0700

    Add files via upload
    possible menu

--- HW 1/source.cpp
+++ HW 1/source.cpp
@@ -13,21 +13,33 @@ algo ag;
 void renderScene(void) {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glBegin(GL_LINES);
-		glVertex2f(-0.5,-0.5);
-		glVertex2f(0.5,0.0);
-		glVertex2f(0.0,0.5);
-	glEnd();
-	coor = {-0.5,-0.5};
+	coor = {-0.75,-0.5};
 	coordinates.push_back(coor);
 	coor = {0.5,0.0};
 	coordinates.push_back(coor);
 	coor = {0.0,0.5};
 	coordinates.push_back(coor);
 	ag.deCastlejau(coordinates);
-
+	coordinates.clear();
   glutSwapBuffers();
 }
+
+void processMenuEvents(int option)
+{
+
+	switch (option)
+	{
+		case 1 :
+			break;
+		case 2 :
+			break;
+		case 3 :
+			break;
+		case 4 :
+			break;
+	}
+}
+
 
 int main(int argc, char **argv) {
 
@@ -37,9 +49,21 @@ int main(int argc, char **argv) {
 	glutInitWindowPosition(400,200);
 	glutInitWindowSize(800,800);
 	glutCreateWindow("Homework 1");
-
 	// register callbacks
 	glutDisplayFunc(renderScene);
+
+	//glutCreateWindow("Options Menu");
+//	glutDisplayFunc(renderScene);
+	int menu = glutCreateMenu(processMenuEvents);
+
+	glutAddMenuEntry("Red",1);
+	glutAddMenuEntry("Blue",2);
+	glutAddMenuEntry("Green",3);
+		glutCreateMenu(processMenuEvents);
+	glutAddSubMenu("Colors", menu);
+
+	glutAddMenuEntry("Exit",4);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	// enter GLUT event processing cycle
 	glutMainLoop();
